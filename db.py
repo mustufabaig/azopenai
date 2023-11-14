3c57c93309c2dcf9c0d3c4876d37608cc5412443
mustufabaig <mustufa.baig@gmail.com>
2023-11-14 08:54:46 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,27 @@
+import streamlit as st
+import json
+import pandas as pd
+from sqlalchemy import create_engine
+
+#loading secrets
+username = st.secrets["username"]
+password = st.secrets["password"]
+warehouse = st.secrets["warehouse"]
+role = st.secrets["role"]
+snowflake_account = st.secrets["account"]
+database = st.secrets["database"]
+schema = st.secrets["schema"]
+        
+#dbsetup
+def get_conn():
+  snowflake_url = f"snowflake://{username}:{password}@{snowflake_account}/{database}/{schema}?warehouse={warehouse}&role={role}"
+  db = create_engine(snowflake_url)
+  return db
+#query and return dataframe
+def query_db(sql):
+  conn = get_conn()
+  df = pd.read_sql_query(sql, conn)
+  return df    
+
+
 
