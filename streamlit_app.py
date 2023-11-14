8986a185c9397c953025e15e263e0a26fd0c6aee
mustufabaig <mustufa.baig@gmail.com>
2023-11-14 12:24:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import db as dbconn
 import pandas as pd
 
 from pandasai import PandasAI
-from pandasai.llm.azure_openai import AzureOpenAI
+from pandasai.llm import AzureOpenAI
 
 openai.api_type = "azure"
 openai.api_base = "https://mbaig-openai.openai.azure.com/"
