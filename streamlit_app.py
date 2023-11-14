c0285d378d0253e3e95d79777030eec2513ce44b
mustufabaig <mustufa.baig@gmail.com>
2023-11-13 21:53:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,20 +10,19 @@ openai.api_version = "2023-07-01-preview"
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 message_text = [{"role":"system","content":"You are an AI assistant who works for payment processor."}]
-question = st.chat_input("How can I help you?")
-message_text.append({"role":"user","content":question})
-
-response = openai.ChatCompletion.create(
-  engine="mbaig-gpt4",
-  messages = message_text,
-  temperature=0.7,
-  max_tokens=800,
-  top_p=0.95,
-  frequency_penalty=0,
-  presence_penalty=0,
-  stop=None
-)
 
 st.set_page_config(layout="wide")
-
-st.write(response)
+question = st.chat_input("How can I help you?")
+if question:
+      message_text.append({"role":"user","content":question})
+      response = openai.ChatCompletion.create(
+        engine="mbaig-gpt4",
+        messages = message_text,
+        temperature=0.7,
+        max_tokens=800,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
+      )
+      st.write(response)
