a9aa3b5e7bab30be9b8f198180b848337595ac63
mustufabaig <mustufa.baig@gmail.com>
2023-11-14 14:40:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,4 @@ if question:
       st.write(question)
       #st.write(pandas_ai.run(df, prompt=question))
       st.write(sdf.chat(question))
+      st.write(sdf.chat(question + ". Plot the histogram of regions using different colors."))
