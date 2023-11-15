8da57e95cc40e250eebcef31b31a9d89e5792852
mustufabaig <mustufa.baig@gmail.com>
2023-11-15 16:50:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,11 +62,11 @@ if question:
 
       if result_json["is_summary"] == "true":
             col1, col2, col3, col4, col5 = st.columns(5)
-            col1.metric("RECEIPT LOOKUPS", df["RECEIPTS_LOOKUPS"].iloc[0], df["RECEIPTS_LOOKUPS_DELTA"].iloc[0].astype(int))
-            col2.metric("REPORTED FRAUD", df["REPORTED_FRAUD"].iloc[0], df["REPORTED_FRAUD_DELTA"].iloc[0].astype(int))
-            col3.metric("REPORTED DISPUTES", df["REPORTED_DISPUTES"].iloc[0], df["REPORTED_DISPUTES_DELTA"].iloc[0].astype(int))
-            col4.metric("FRAUD/DISPUTE NOT DEFLECTED", df["FRAUD_DISPUTE_NOT_DEFLECTED"].iloc[0], df["FRAUD_DISPUTE_NOT_DEFLECTED_DELTA"].iloc[0].astype(int))
-            col5.metric("FRAUD/DISPUTE DEFLECTED", df["FRAUD_DISPUTE_DEFLECTED"].iloc[0], df["FRAUD_DISPUTE_DEFLECTED_DELTA"].iloc[0].astype(int))
+            col1.metric("RECEIPT LOOKUPS", df["RECEIPTS_LOOKUPS"].iloc[0], str(df["RECEIPTS_LOOKUPS_DELTA"].iloc[0]))
+            col2.metric("REPORTED FRAUD", df["REPORTED_FRAUD"].iloc[0], str(df["REPORTED_FRAUD_DELTA"].iloc[0]))
+            col3.metric("REPORTED DISPUTES", df["REPORTED_DISPUTES"].iloc[0], str(df["REPORTED_DISPUTES_DELTA"].iloc[0]))
+            col4.metric("FRAUD/DISPUTE NOT DEFLECTED", df["FRAUD_DISPUTE_NOT_DEFLECTED"].iloc[0], str(df["FRAUD_DISPUTE_NOT_DEFLECTED_DELTA"].iloc[0]))
+            col5.metric("FRAUD/DISPUTE DEFLECTED", df["FRAUD_DISPUTE_DEFLECTED"].iloc[0], str(df["FRAUD_DISPUTE_DEFLECTED_DELTA"].iloc[0]))
 
       #pandaai
       #st.write('pandasai')
