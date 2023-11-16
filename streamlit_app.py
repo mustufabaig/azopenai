4dbd9b7d247526d53a9ce95f40a6646521867dd5
mustufabaig <mustufa.baig@gmail.com>
2023-11-16 07:42:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,13 +45,15 @@ if question:
       collected_events = []
       completion_text = ''
       for event in response:
+            st.write(event)
+            '''
             event_time = time.time() - start_time  # calculate the time delay of the event
             collected_events.append(event)  # save the event response
-            event_text = event['choices'] #[0]['text']  # extract the text
+            event_text = event['choices'][0]['text']  # extract the text
             completion_text += event_text  # append the text
             st.write(f"Text received: {event_text} ({event_time:.2f} seconds after request)")  # print the delay and text
-
+            '''
       # print the time delay and text received
-      st.write(f"Full response received {event_time:.2f} seconds after request")
-      st.write(f"Full text received: {completion_text}")
+      #st.write(f"Full response received {event_time:.2f} seconds after request")
+      #st.write(f"Full text received: {completion_text}")
 
